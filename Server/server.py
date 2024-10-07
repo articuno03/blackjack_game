13d6ad231ee69381c5520d0cb494e288080b0486
willfagerstrom <99782280+willfagerstrom@users.noreply.github.com>
2024-10-06 23:12:54 -0600

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,1 +1,41 @@
 
+import socket
+import selectors
+
+sel = selectors.DefaultSelector()
+
+def accept(sock, mask):
+    conn, addr = sock.accept()  
+    print('accepted connection from', addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read)
+
+def read(conn, mask):
+    data = conn.recv(1024)
+    if data:
+        print('echoing', repr(data), 'to', conn)
+        conn.send(data)  # Hope it won't block
+    else:
+        print('closing', conn)
+        sel.unregister(conn)
+        conn.close()
+
+host = '' 
+port = 65432
+
+sock = socket.socket()
+sock.bind((host, port))
+sock.listen(100)
+sock.setblocking(False)
+sel.register(sock, selectors.EVENT_READ, accept)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
