ca4c49399fee0df693d3a1201485cc3de90739bc
Garret Tilton <garrett6648@gmail.com>
2024-11-25 13:08:28 -0700

    Significant changes and code cleanup

--- Server/blackjack_game.py
+++ Server/blackjack_game.py
@@ -2,61 +2,87 @@ import json
 
 class BlackjackGame:
     def __init__(self, players):
-        """
-        Initialize the game with a list of players.
-        :param players: List of player dictionaries with their connection and username.
-        """
         self.players = players
-        self.deck = self.create_deck()  # Initialize a deck of cards
-        self.hands = {player['username']: [] for player in players}  # Store player hands
+        self.deck = self.create_deck()
+        self.hands = {player['username']: [] for player in players}
+        self.turn_order = [player['username'] for player in players]
+        self.current_player = 0
+        self.busted_players = set()
 
     def create_deck(self):
-        """Creates a standard 52-card deck."""
         suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         return [f"{rank} of {suit}" for suit in suits for rank in ranks]
 
     def shuffle_deck(self):
-        """Shuffles the deck."""
         import random
         random.shuffle(self.deck)
 
     def deal_initial_cards(self):
-        """Deals two cards to each player."""
         self.shuffle_deck()
         for player in self.players:
             username = player['username']
             self.hands[username] = [self.deck.pop(), self.deck.pop()]
 
-    def send_initial_hands(self):
-        """Sends each player their own cards."""
-        for player in self.players:
-            conn = player['conn']
-            username = player['username']
-            hand = self.hands[username]
-            try:
-                conn.send(json.dumps({
-                    "type": "game",
-                    "content": f"Your initial hand: {', '.join(hand)}"
-                }).encode())
-                print(f"Sent hand to {username}: {', '.join(hand)}")
-            except BrokenPipeError:
-                print(f"Failed to send hand to {username}. Connection might be closed.")
-
-    def broadcast_game_start(self):
-        """Notify players that the game has started and send initial hands."""
-        for player in self.players:
-            conn = player['conn']
-            username = player['username']
-            conn.send(json.dumps({
-                "type": "game",
-                "content": f"Game started! Your hand: {self.hands[username]}"
-            }).encode())
+    def send_hand(self, conn, username):
+        hand = self.hands[username]
+        conn.send(json.dumps({
+            "type": "game",
+            "content": f"Your hand: {', '.join(hand)}"
+        }).encode())
 
     def start_game(self):
-        """Starts the game logic."""
-        print("Blackjack game is starting...")
         self.deal_initial_cards()
-        self.broadcast_game_start()
+        for player in self.players:
+            conn, username = player['conn'], player['username']
+            self.send_hand(conn, username)
+        self.prompt_next_player()
 
-        # You can add more logic for gameplay here.
+    def prompt_next_player(self):
+        if self.current_player < len(self.turn_order):
+            current_username = self.turn_order[self.current_player]
+            self.broadcast_message({
+                "type": "info",
+                "content": f"It's {current_username}'s turn."
+            })
+
+    def handle_player_action(self, conn, action):
+        username = self.turn_order[self.current_player]
+
+        if action == "hit":
+            card = self.deck.pop()
+            self.hands[username].append(card)
+            self.send_hand(conn, username)
+
+            if self.calculate_hand_value(self.hands[username]) > 21:
+                self.busted_players.add(username)
+                conn.send(json.dumps({
+                    "type": "info",
+                    "content": "You busted!"
+                }).encode())
+                self.current_player += 1
+                self.prompt_next_player()
+
+        elif action == "stand":
+            self.current_player += 1
+            self.prompt_next_player()
+
+    def calculate_hand_value(self, hand):
+        value = 0
+        aces = 0
+
+        for card in hand:
+            rank = card.split(' ')[0]
+            if rank.isdigit():
+                value += int(rank)
+            elif rank in ['J', 'Q', 'K']:
+                value += 10
+            elif rank == 'A':
+                value += 11
+                aces += 1
+
+        while value > 21 and aces:
+            value -= 10
+            aces -= 1
+
+        return value
